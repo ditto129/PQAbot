de5be6efca4af0a41ef6ef7f10c613f730215c73
Patty <L224749391@gmail.com>
2021-06-08 21:04:29 +0800

    要填入整句話當slot時，加前綴

--- views/base_flow_rasa_api.py
+++ views/base_flow_rasa_api.py
@@ -18,51 +18,24 @@ base_flow_rasa_api=Blueprint('base_flow_rasa_api', __name__)
 @base_flow_rasa_api.route('base_flow_rasa')
 def base_flow_rasa():
     sender_id=request.values.get('sender_id')
-    print('sender_id'+sender_id)
     message=request.values.get('message')
-    #translatedMessage=Translate(message).getTranslate()
-    #print(translatedMessage)
+    
+    conversations_tracker = requests.get('http://localhost:5005/conversations/'+sender_id+'/tracker')
+    results = json.loads(conversations_tracker.content.decode('utf8'))
+    if len(results['events'])>=2:
+        print(results['events'][-2]['text'])
+        lastest_bot_reply=results['events'][-2]['text']
+        if lastest_bot_reply == "請描述您遇到的問題" or lastest_bot_reply == "請貼上您的錯誤訊息":
+            message = 'question_or_error_message' + message
+        
     payload = {'sender': sender_id, 'message': message}
     headers = {'content-type': 'application/json'}
+    
     r = requests.post('http://localhost:5005/webhooks/rest/webhook', json=payload, headers=headers)
+    
     print(r.json())
     if len(r.json()) == 0:
         return jsonify({"message":"no triggered intent"})
     else:
         return r.json()[0]
         
-@base_flow_rasa_api.route('chat_accepted', methods=['post'])
-def chat_accepted():
-    param=request.get_json()
-    print('chat_accepted')
-    print('event_name: '+ param['event_name'])
-    print('chat_id: '+str(param['chat_id']))
-    return jsonify({"message":"success"})
-    
-#https://wh.jivosite.com/IF6YK0nYgC56npYB/vKIgQjkZ0T
-
-@base_flow_rasa_api.route('process', methods=['post'])
-def process():
-    param=request.get_json()
-    print('process')
-    print('event: '+ param['event'])
-    print('id: '+str(param['id']))
-    return jsonify({
-  "event": "BOT_MESSAGE",
-  "id": "123e4567-e89b-12d3-a456-426655440000",
-  "message": {
-    "type": "BUTTONS",
-      "title": "Are you interested in delivery within the New York area?",
-      "text": "Are you interested in delivery within the New York area? Yes / No",
-      "timestamp": "1583910736",
-      "buttons": [
-        {
-          "text": "Yes",
-          "id": "1"
-        }, {
-          "text": "No",
-          "id": "2"
-        }
-      ]
-  }
-})
