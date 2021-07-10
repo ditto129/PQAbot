891a575a6ce7233d4d7cf43457a854e67bb2f915
Patty <L224749391@gmail.com>
2021-07-11 01:32:59 +0800

    完成篩選貼文頁面

--- static/assets/js/pages/menuBarJS/menuBar.js
+++ static/assets/js/pages/menuBarJS/menuBar.js
@@ -15,7 +15,7 @@ function changePage(){
     var content = "";
     content += '<iframe MARGINWIDTH=0 MARGINHEIGHT=0 HSPACE=0 VSPACE=0 frameborder=0 scrolling=auto src="';
     content += page;
-    content += '.html"  height="100%" width="100%"></iframe>';
+    content += '" height="100%" width="100%"></iframe>';
     document.getElementById("main_page").innerHTML = content;
 
 }
