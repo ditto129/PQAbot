d6577cbb4ca474b3bdca64e842a730e9fcd5be2c
Patty <L224749391@gmail.com>
2021-07-12 00:58:01 +0800

    change skill tree position

--- static/assets/js/common.js
+++ static/assets/js/common.js
@@ -1,2 +1,2 @@
-var head_url = "http://0.0.0.0:55001/";
-//var head_url = "http://140.121.197.130:55001/";
+//var head_url = "http://0.0.0.0:55001/";
+var head_url = "http://140.121.197.130:55001/";
