6f17b8917ebd3811b0bd40fa6994aa613bbd5089
ditto129 <57188733+ditto129@users.noreply.github.com>
2021-07-11 16:56:44 +0800

    inner_post,post_api

--- views/user_api.py
+++ views/user_api.py
@@ -6,56 +6,80 @@ from flask import Blueprint, request, jsonify
 
 # --- our models ---- #
 from models import user
-#from models.psa_excption_handler import err_msg
+# rom models import psa_excption_handler
+
 
 user_api = Blueprint("user_api", __name__)
 
 # 取得使用者簡易資料，不包含技能樹、發文紀錄
-@user_api.route('/get_user_profile', methods=['POST'])
-def get_user_profile():
-    data = request.get_json()
+@user_api.route('/query_user_profile', methods=['POST'])
+def query_user_profile():
+    data = request.query_json()
     user_dict = user.query_user(data['id'])
     try:
         user_profile = {
             '_id' : user_dict['id'],
             'name': user_dict['name'],
             'email':user_dict['email'],
-            'exp' : user_dict['exp']
+            'img': user_dict['img']
         }
-    except Exception as e:
-        user_profile = {"error" : err_msg(e)}
-    jsonify(user_profile),200
+    except Exception as e :
+        user_profile = {"error" : e.__class__.__name__ + e.args[0]}
+        print(e)
+    return jsonify(user_profile),200
 
 
 # 取得使用者發文紀錄
-@user_api.route('/get_user_post_list', methods=['POST'])
-def get_user_post_list():
-    data = request.get_json()
+@user_api.route('/query_user_post_list', methods=['POST'])
+def query_user_post_list():
+    data = request.query_json()
     user_dict = user.query_user(data['id'])
     try:
         user_posts = user_dict['record']['posts']
-    except Exception as e:
-        user_posts = {"error" : err_msg(e)}
-    jsonify(user_posts),200
+    except Exception as e :
+        user_posts = {"error" : e.__class__.__name__ + e.args[0]}
+        print(e)
+    return jsonify(user_posts),200
     
 # 取得使用者回覆紀錄
-@user_api.route('/get_user_response_list', methods=['POST'])
-def get_user_response_list():
-    data = request.get_json()
+@user_api.route('/query_user_response_list', methods=['POST'])
+def query_user_response_list():
+    data = request.query_json()
     user_dict = user.query_user(data['id'])
     try:
         user_posts = user_dict['record']['responses']
-    except Exception as e:
-        user_posts = {"error" : err_msg(e)}
-    jsonify(user_posts),200
+    except Exception as e :
+        user_posts = {"error" : e.__class__.__name__ + e.args[0]}
+        print(e)
+    return jsonify(user_posts),200
 
 # 取得使用者技能樹
-@user_api.route('/get_user_skill', methods=['POST'])
-def get_user_skill():
-    data = request.get_json()
+@user_api.route('/query_user_skill', methods=['POST'])
+def query_user_skill():
+    data = request.query_json()
     user_dict = user.query_user(data['id'])
     try:
         user_skill = user_dict['skill']
-    except Exception as e:
-        user_skill = {"error" : err_msg(e)}
-    jsonify(user_skill),200
+    except Exception as e :
+        user_skill = {"error" : e.__class__.__name__ + e.args[0]}
+        print(e)
+    return jsonify(user_skill),200
+    
+# 編輯使用者簡易資料，不包含技能樹、發文紀錄
+@user_api.route('/update_user_profile', methods=['POST'])
+def update_user_profile():
+    data = request.query_json()
+    try:
+        user_profile = {
+            '_id' : data['id'],
+            'name': data['name'],
+            'email':data['email'],
+            'img': data['img']
+        }
+        user.update_user(user_profile)
+    except Exception as e :
+        user_profile = {"error" : e.__class__.__name__ + e.args[0]}
+        print(e)
+    return jsonify(user_profile),200
+    
+
